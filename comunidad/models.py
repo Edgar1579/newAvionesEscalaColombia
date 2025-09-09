96e24d085b3d179fb684ab1ac19f51619012df81
Edgar1579 <es535965@gmail.com>
2025-09-09 17:33:30 -0500

    add class Usuario

--- comunidad/models.py
+++ comunidad/models.py
@@ -1,12 +1,41 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
+    primer_nombre = models.CharField(max_length=45, verbose_name="Primer Nombre")
+    segundo_nombre = models.CharField(max_length=45, verbose_name="Segundo Nombre", blank=True, null=True)
+    primer_apellido = models.CharField(max_length=45, verbose_name="Primer Apellido")
+    segundo_apellido = models.CharField(max_length=45, verbose_name="Segundo Apellido")
     email = models.EmailField(unique=True)
     telefono = models.CharField(max_length=15, blank=True, null=True)
     fecha_registro = models.DateTimeField(auto_now_add=True)
 
+    class Rol(models.TextChoices):
+        ADMINISTRADOR = 'AD', _('Administrador')
+        CLIENTES = 'CL', _('Cliente')
+        EMPLEADOS = 'EM', _('Empleado')
+    rol=models.CharField(max_length=2, choices=Rol.choices,default=Rol.choices.CLIENTES,verbose_name="Rol")
+
+    class TipoDocumento(models.TextChoices):
+            CEDULA = 'CC', _("Cédula")
+            CEDULA_EXTRANJERIA = 'CE', _("Cédula de Extrangería")
+
+    tipo_documento = models.CharField(max_length=2, choices=TipoDocumento.choices, verbose_name="Tipo de Documento")
+    documento = models.PositiveIntegerField(verbose_name="Documento", unique=True)        
+
+    def clean(self):
+            self.primer_nombre = self.primer_nombre.title() 
+
     def __str__(self):
-        return self.nombre
+        return f"{self.primer_nombre} {self.primer_apellido}"
+    class Meta:
+        verbose_name_plural = "Usuarios"
+        
+    @property
+    def full_name(self):
+        if self.segundo_nombre:
+            return f"{self.primer_nombre} {self.segundo_nombre} {self.primer_apellido} {self.segundo_apellido}"
+        else:
+            return f"{self.primer_nombre} {self.primer_apellido} {self.segundo_apellido}"      
     
