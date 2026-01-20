6483e5aaa7a84cf79a527da98385318d0b077fa0
Edgar1579 <es535965@gmail.com>
2026-01-19 19:54:25 -0500

    mejoras en carrito add map en el footer

--- operaciones/context_processors.py
+++ operaciones/context_processors.py
@@ -1,4 +1,17 @@
-from .views import obtener_carrito
-
 def carrito_context(request):
-    return obtener_carrito(request)
+    carrito = request.session.get('carrito', {})
+    total = 0
+    cantidad_items = 0
+
+    for item in carrito.values():
+        precio = float(item.get('precio', 0))
+        cantidad = int(item.get('cantidad', 0))
+        item['subtotal'] = precio * cantidad
+        total += item['subtotal']
+        cantidad_items += cantidad
+
+    return {
+        'carrito': carrito,
+        'total': round(total, 2),
+        'cantidad_items': cantidad_items
+    }
